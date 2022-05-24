1b21a426aadf5e58b600baed0ff3f5ebaae1620f
Marek Arendt <marek.arendt@yougov.com>
2022-05-24 15:50:45 +0200

    Project initialized.

---
+++ test_issues/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-r7-hb5skvbb!m+r#6o1yk3j3tsw6yh&+9n*!dcvpgi1d7x3(&^'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
